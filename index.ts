8ed95f2866130f0ea8393362ed349c9c174c676c
david uribe <uribesotod@gmail.com>
2023-11-19 12:42:53 -0500

    Initial Commit

--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const lambdaRole = new aws.iam.Role("lambdaRole", {
+    assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "sts:AssumeRole",
+            Principal: {
+                Service: "lambda.amazonaws.com",
+            },
+            Effect: "Allow",
+            Sid: "",
+        }],
+    }),
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+new aws.iam.RolePolicyAttachment("lambdaFullAccess", {
+    role: lambdaRole.name,
+    policyArn: "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
+});
+
+// Create the Lambda function
+new aws.lambda.Function("HelloWorld", {
+    runtime: aws.lambda.NodeJS12dXRuntime,
+    handler: "index.handler",
+    role: lambdaRole.arn
+});
+
+
+
+
