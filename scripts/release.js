baf0734fd05537b4a29ef8dd9e6ec88b509534f8
david uribe <uribesotod@gmail.com>
2023-12-27 23:14:44 -0500

    Creating new release

--- scripts/release.js
+++ scripts/release.js
@@ -2,6 +2,7 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 
 const version = require("../package.json").version;
+const packageName = require("../package.json").name;
 
 // Create a GitHub release
 execSync(`gh release create v${version} -t "Release v${version}"`);
@@ -9,11 +10,16 @@ execSync(`gh release create v${version} -t "Release v${version}"`);
 // Create a tarball of your package
 execSync("npm pack");
 
-// Create the releases folder if it doesn't exist
-if (!fs.existsSync("./releases")) {
-  fs.mkdirSync("./releases");
-}
+// Move the tarball to the releases folder only if it exists
+const tarballName = `${packageName}-${version}.tgz`;
+const sourcePath = `./${tarballName}`;
+const destinationPath = `./releases/${tarballName}`;
 
-// Move the tarball to a releases directory
-const tarballName = `@washmeapp/infra-${version}.tgz`;
-fs.renameSync(`./${tarballName}`, `./releases/${tarballName}`);
+// Check if the tarball exists before moving it
+if (fs.existsSync(sourcePath)) {
+  // Move the tarball to the releases folder
+  fs.renameSync(sourcePath, destinationPath);
+} else {
+  // Handle the case where the tarball doesn't exist
+  console.error(`Error: Tarball '${tarballName}' does not exist.`);
+}
