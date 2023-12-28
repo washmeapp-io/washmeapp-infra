a13811cdbc1af0d25b3955bf6bab6eb6c46e6cd0
david uribe <uribesotod@gmail.com>
2023-12-27 23:02:35 -0500

    Initial commit

--- scripts/release.js
+++ scripts/release.js
@@ -9,6 +9,11 @@ execSync(`gh release create v${version} -t "Release v${version}"`);
 // Create a tarball of your package
 execSync("npm pack");
 
+// Create the releases folder if it doesn't exist
+if (!fs.existsSync("./releases")) {
+  fs.mkdirSync("./releases");
+}
+
 // Move the tarball to a releases directory
 const tarballName = `@washmeapp/infra-${version}.tgz`;
 fs.renameSync(`./${tarballName}`, `./releases/${tarballName}`);
