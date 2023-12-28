c15e5a5cd253f817073536f32c6f68766c551173
david uribe <uribesotod@gmail.com>
2023-12-27 22:21:18 -0500

    Initial commit

--- index.ts
+++ index.ts
@@ -1,31 +1,3 @@
-import * as aws from "@pulumi/aws";
+import * as lambda from "./src/lambdas";
 
-const lambdaRole = new aws.iam.Role("lambdaRole", {
-    assumeRolePolicy: JSON.stringify({
-        Version: "2012-10-17",
-        Statement: [{
-            Action: "sts:AssumeRole",
-            Principal: {
-                Service: "lambda.amazonaws.com",
-            },
-            Effect: "Allow",
-            Sid: "",
-        }],
-    }),
-});
-
-new aws.iam.RolePolicyAttachment("lambdaFullAccess", {
-    role: lambdaRole.name,
-    policyArn: "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
-});
-
-// Create the Lambda function
-new aws.lambda.Function("HelloWorld", {
-    runtime: aws.lambda.NodeJS12dXRuntime,
-    handler: "index.handler",
-    role: lambdaRole.arn
-});
-
-
-
-
+export { lambda };
