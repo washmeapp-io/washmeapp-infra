0b717b7a7e05fdccb3253f57c548fdbfdc0c869d
david uribe <uribesotod@gmail.com>
2023-12-29 12:51:58 -0500

    Creating new dev stack with pulumi

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
-import * as lambda from "./src/lambdas";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 
-export { lambda };
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket("my-bucket");
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
--- src/lambdas/index.ts
+++ src/lambdas/index.ts
@@ -1,11 +1,16 @@
 import * as aws from "@pulumi/aws";
 import { lambdaRole } from "../roles";
+import { BucketObject } from "@pulumi/aws/s3";
 
-export function createLambdaFunction(name: string): aws.lambda.Function {
+export function createLambdaFunction(
+  name: string,
+  code: BucketObject
+): aws.lambda.Function {
   const lambda = new aws.lambda.Function(name, {
     runtime: aws.lambda.NodeJS12dXRuntime,
     handler: "index.handler",
     role: lambdaRole.arn,
+    code,
   });
 
   return lambda;
