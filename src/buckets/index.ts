ad2e5050122cad0b733e9c7be2541dc3d1db9b79
david uribe <uribesotod@gmail.com>
2024-01-13 19:13:56 -0500

    Update bucket name

--- src/buckets/index.ts
+++ src/buckets/index.ts
@@ -1,9 +1,5 @@
 import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
 
 export const lambdaCode = new aws.s3.BucketObject("washmeapp-backend-code", {
-  bucket: "myBucket", // replace this with your bucket name
-  source: new pulumi.asset.RemoteArchive(
-    "https://github.com/user/repo/archive/refs/heads/main.zip"
-  ),
+  bucket: "washmeapp-backend-code",
 });
