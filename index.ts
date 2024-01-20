de6b508b1c37269fc86ef2b669888244015b135b
david uribe <uribesotod@gmail.com>
2024-01-20 06:31:26 -0500

    Manual dispatch for deploy pulumi

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as aws from "@pulumi/aws";
+
 import * as buckets from "./src/buckets";
 import * as lambda from "./src/lambdas";
 import * as utils from "./src/utils";
@@ -7,11 +9,15 @@ const userBucket = buckets.createBucket({
   name: "washmeapp-code",
   provider: provider,
 });
-userBucket.onObjectCreated("users-api", () => {
+userBucket.onObjectCreated("users-api", async () => {
+  const usersApiCode = await aws.s3.getBucketObject({
+    bucket: "washmeapp-code",
+    key: "users-api",
+  });
   lambda.createLambdaFunction({
     name: "washmeapp-api-users",
     bucket: userBucket,
     provider: provider,
-    bucketKey: "users-api",
+    bucketKey: usersApiCode.key,
   });
 });
