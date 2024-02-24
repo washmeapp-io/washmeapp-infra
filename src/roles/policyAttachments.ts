92ca91ad04e424f929e724c3eb98017bf3a423fb
david uribe <uribesotod@gmail.com>
2024-02-23 21:46:08 -0500

    using apply

--- src/roles/policyAttachments.ts
+++ src/roles/policyAttachments.ts
@@ -2,32 +2,35 @@ import * as aws from "@pulumi/aws";
 import { lambdaRole } from "./roles";
 import { Output } from "@pulumi/pulumi";
 
-export function assignLambdaCognitoPolicy(userPoolArn: Output<string>) {
-  // Define a policy document for accessing Cognito
-  const cognitoPolicyDocument = aws.iam
-    .getPolicyDocument({
-      statements: [
-        {
-          effect: "Allow",
-          actions: [
-            "cognito-idp:AdminCreateUser",
-            // Add any other Cognito actions your Lambda needs here
-          ],
-          resources: [
-            // Replace with your Cognito User Pool ARN
-            userPoolArn.get(),
-          ],
-        },
-      ],
-    })
-    .then((policyDoc) => policyDoc.json);
+export function assignLambdaCognitoPolicy(
+  userPoolArn: Output<string>,
+  lambdaRoleName: string
+) {
+  userPoolArn.apply((arn) => {
+    const cognitoPolicyDocument = aws.iam
+      .getPolicyDocument({
+        statements: [
+          {
+            effect: "Allow",
+            actions: [
+              "cognito-idp:AdminCreateUser",
+              // Add any other Cognito actions your Lambda needs here
+            ],
+            resources: [arn],
+          },
+        ],
+      })
+      .then((policyDoc) => policyDoc.json);
 
-  const cognitoPolicy = new aws.iam.Policy("cognitoAccessPolicy", {
-    policy: cognitoPolicyDocument,
-  });
+    const cognitoPolicy = new aws.iam.Policy("cognitoAccessPolicy", {
+      policy: cognitoPolicyDocument,
+    });
 
-  new aws.iam.RolePolicyAttachment("lambdaCognitoPolicyAttachment", {
-    role: lambdaRole.name,
-    policyArn: cognitoPolicy.arn,
+    cognitoPolicy.arn.apply((policyArn) => {
+      new aws.iam.RolePolicyAttachment("lambdaCognitoPolicyAttachment", {
+        role: lambdaRoleName,
+        policyArn: policyArn,
+      });
+    });
   });
 }
